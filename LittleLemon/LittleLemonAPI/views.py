b32bd2039723444ccf8da60095d140238fb45490
em4n0n <58372945+em4n0n@users.noreply.github.com>
2024-10-31 17:45:41 -0700

    Add userratethrottle and anonratethrottle

--- LittleLemon/LittleLemonAPI/views.py
+++ LittleLemon/LittleLemonAPI/views.py
@@ -2,8 +2,11 @@ from rest_framework.response import Response
 from rest_framework import viewsets
 from .models import MenuItem
 from .serializers import MenuItemSerializer
+from rest_framework.throttling import UserRateThrottle, AnonRateThrottle
+
 # Create your views here.
 
 class MenuItemsViewSet(viewsets.ModelViewSet):
+    throttle_classes = [AnonRateThrottle, UserRateThrottle]
     queryset = MenuItem.objects.all()
     serializer_class = MenuItemSerializer
