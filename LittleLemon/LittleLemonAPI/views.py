8a8b41fec5158b7b22ecaa2b0b3ad62514d74cb8
em4n0n <58372945+em4n0n@users.noreply.github.com>
2024-11-03 19:55:57 -0800

    implement conditional throttling

--- LittleLemon/LittleLemonAPI/views.py
+++ LittleLemon/LittleLemonAPI/views.py
@@ -7,6 +7,12 @@ from rest_framework.throttling import UserRateThrottle, AnonRateThrottle
 # Create your views here.
 
 class MenuItemsViewSet(viewsets.ModelViewSet):
-    throttle_classes = [AnonRateThrottle, UserRateThrottle]
     queryset = MenuItem.objects.all()
     serializer_class = MenuItemSerializer
+    
+    def get_throttles(self):
+        if self.action == 'create':
+            throttle_classes = [UserRateThrottle]
+        else:
+            throttle_classes = []
+        return [throttle() for throttle in throttle_classes]
